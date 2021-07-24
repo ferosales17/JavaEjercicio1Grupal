9c9ed25192ff36a552c77a7a600bf4fcf313b9e7
Federico Nicolas Rosales Cabrera <ferosales@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-07-24 18:27:55 -0300

    hasta acà llego con el 5 me duele mucho la cabeza

---
+++ src/Test/Ejercicio5Test.java
@@ -0,0 +1,15 @@
+package Test;
+
+import static domain.Ejercicio5.*;
+
+public class Ejercicio5Test {
+
+    public static void main(String[] args) {
+        //contarDigitos(200);
+        recorrer(5,1,2);
+        //buscarNumero(32,2);
+
+    }
+
+
+}
--- src/domain/Ejercicio5.java
+++ src/domain/Ejercicio5.java
@@ -1,9 +1,11 @@
 package domain;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingrese el primer numero:"); // numero n -> los primero numeros
@@ -12,9 +14,71 @@ public class Ejercicio5 {
         System.out.println("Ingrese el segundo numero:"); // m -> cantidad de digitos
         int m = scanner.nextInt();
 
-        System.out.println("Ingrese el tercer numero:"); // d -> digitos
+        System.out.println("Ingrese el tercer numero:"); // d -> numero que busco
         int d = scanner.nextInt();
 
+        scanner.close();
 
+    }
+
+    //n ,digitos,numeroque busco
+    public static void recorrer(int a, int d, int b) {
+        //int numeroGrande = Integer.MAX_VALUE; // me trae el numero maximo de integer= 2147483647 y lo asigno
+        int numeroGrande = 30;
+        String numeroGrandeString = String.valueOf(numeroGrande);
+
+        if (a < 0) {
+            System.out.println("No debe ser un numero negativo");
+        } else {
+            for (int i = 0; i < numeroGrande; i++) {
+                String numeroEncontrado = buscarNumero(i, b);
+                String bString = String.valueOf(b);
+                if (numeroEncontrado.contains(bString)) {
+                    int numEncInt = Integer.parseInt(numeroEncontrado);
+                    //String resultCon = contarDigitos(numEncInt);
+                    String nDigitos = String.valueOf(d);
+
+                    String numEcontradoString = String.valueOf(numEncInt);
+                    for (int j = 0; j < numEcontradoString.length(); j++) {
+                        String jstring = String.valueOf(numEcontradoString.charAt(j));
+                        int cont2 = 0;
+                        if (jstring.equals(bString)) {
+                            cont2++;
+                        }
+                        String contadorNumQ = String.valueOf(cont2);
+                        if (contadorNumQ.equals(nDigitos)) {
+                            System.out.println("numero econtrado = " + numEcontradoString);
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    public static String contarDigitos(int b) {
+        int count = 0;
+        for (int i = 0; i < b; i++) {
+            while (b != 0) {
+                b /= 10;
+                count++;
+            }
+
+        }
+
+        String cantidad = String.valueOf(count);
+        return cantidad;
+    }
+
+    //numero,d
+    public static String buscarNumero(int c, int d) {
+        String numeroIngreso = String.valueOf(d);
+        String numeroString = String.valueOf(c);
+        if (numeroString.contains(numeroIngreso) == true) {
+            return numeroString;
+        }
+
+        return numeroString;
     }
 }
